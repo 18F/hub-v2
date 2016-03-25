1f37ae65f6a989a45ee0bb7f8bb4d160df680d20
mtorres253 <michael.torres@gsa.gov>
2016-03-24 17:06:37 -0700

    added a comment to application.rb about turning off precompiling, added listen_here to environmet.rb

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module HubV2
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Added this to prevent cloud.gov from trying to precompile assets, then failing - MT
     config.assets.initialize_on_precompile = false
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#set the port to PORT
+listen_here = ENV['PORT']
