e26d70b298302295710fb4b4a2710f0cf8a98bab
mtorres253 <michael.torres@gsa.gov>
2016-03-24 17:38:05 -0700

    Trying again to change the default port to ENV[PORT]

--- config/boot.rb
+++ config/boot.rb
@@ -2,5 +2,12 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
-#listen at PORT ENV variable
-listen_here = ENV['PORT']
+require'rails/commands/server'
+module Rails
+  class Server
+    alias:default_options_alias :default_options
+    def default_options
+      default_options_alias.merge!(:Port=>ENV['PORT'])
+    end
+  end
+end
