65ad1f183a11ac6172d862fca7a0d9f069c11124
jbarnicle <michael.barnicle@gsa.gov>
2016-04-26 12:42:05 -0500

    new hires integration / search results updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
+require 'team_api'
+
 class HomeController < ApplicationController
+
+  include TeamApi
   
   def index
+
   	@bodyClass = 'layout-homepage';
+    @recentHires = get_recent_hires();
+
   end
 
 end
