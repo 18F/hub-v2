3a8fde0a1be7f7e44e0418cee559b630e8620bcf
jbarnicle <michael.barnicle@gsa.gov>
2016-05-06 13:41:30 -0400

    make api values configurable

--- lib/cached_api.rb
+++ lib/cached_api.rb
@@ -1,18 +1,20 @@
 module CachedApi
 
 	def get_recent_hires
-	  
-	  Rails.cache.fetch("recent_hires", :expires_in => 60.minutes) do
 
-	  	logger.info "refreshing cache" 
+	  team_api_url = ENV['TEAM_API_URL']
+	  cache_refresh = ENV['TEAM_API_CACHE_REFRESH_MINUTES'].to_i 
+	  new_hire_window = ENV['TEAM_API_NEW_HIRE_WINDOW_DAYS'].to_i 
+
+	  Rails.cache.fetch("recent_hires", :expires_in => cache_refresh.minutes) do
+
 	  	recent_hires = Array.new;
-	  	url = 'https://team-api.18f.gov/public/api/team/';
-	  	response = HTTParty.get(url);
+	  	response = HTTParty.get(team_api_url);
     	apidata = JSON.parse(response.body);
     	apidata['results'].each do |person|
     		if (person['start_date'] != nil)
     			hire_date = DateTime.parse(person['start_date']);
-    			if (hire_date > Time.now - 40.days)
+    			if (hire_date > Time.now - new_hire_window.days)
     				person['has_photo'] = check_for_photo(person['name'])
     				recent_hires << person
     			end
